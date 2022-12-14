605626ee12ffeff677389fdf95bd26323c9b5ba3
Apoorv Goyal <apoorvgoyal@Apoorvs-MacBook-Pro.local>
2022-12-14 09:50:20 +0530

    changed db path

--- storage.py
+++ storage.py
@@ -5,19 +5,19 @@ import random
 # Can be replaced with respective database functions to store against respective products
 
 def storeData(id,data):
-    s = shelve.open( "database/data" )
+    s = shelve.open( "data" )
     s[str(id)]=data
     s.close()
 
 
 def retreiveData(id):
-    s = shelve.open( "database/data" )
+    s = shelve.open( "data" )
     data = s[str(id)]
     s.close()
     return data
 
 def getRandomData():
-    s = shelve.open( "database/data" )
+    s = shelve.open( "data" )
     randomKey = random.choice(s.keys())
     s.close()
     return randomKey
